7cd62b795eb62ebc430f7a941315e807dc251a0a
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-01-30 18:49:28 +0530

    Add files via upload

---
+++ January 2024/Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 + num2);
+            } else if (token.equals("-")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 - num2);
+            } else if (token.equals("*")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 * num2);
+            } else if (token.equals("/")) {
+                int num2 = stack.pop();
+                int num1 = stack.pop();
+                stack.push(num1 / num2);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+}
