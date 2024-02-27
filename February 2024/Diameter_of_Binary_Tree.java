5334e21ee3ca423f800fa25c0e1f9e6f785e779c
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-02-27 08:45:02 +0530

    Add files via upload

---
+++ February 2024/Diameter_of_Binary_Tree.java
@@ -0,0 +1,24 @@
+lass Solution {
+    public int heightOfTree(TreeNode root){ //max nodes in one line
+       if(root == null){
+           return 0;
+       }
+
+       int leftheight=heightOfTree(root.left);
+       int rightheight=heightOfTree(root.right);
+       int ans=Math.max(leftheight,rightheight) + 1;
+
+       return ans;
+   }
+   public int diameterOfBinaryTree(TreeNode root) {
+       if(root == null){
+           return 0;
+       }
+
+       int op1=diameterOfBinaryTree(root.left);
+       int op2=diameterOfBinaryTree(root.right);
+       int op3=heightOfTree(root.left)+heightOfTree(root.right);
+       int ans=Math.max(op1,Math.max(op2,op3));
+       return ans;
+   }
+}
