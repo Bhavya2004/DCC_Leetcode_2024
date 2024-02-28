225512cd9fe219cb0e872402c60384f32f401989
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-02-28 11:59:12 +0530

    Add files via upload

---
+++ February 2024/Find_Bottom_Left_Tree_Value.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        if (root == null) {
+            throw new IllegalArgumentException("root is null");
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int leftmostValue = root.val;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            leftmostValue = queue.peek().val;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return leftmostValue;
+    }
+}
