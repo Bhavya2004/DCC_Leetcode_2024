b946a3580e973c692571a3953e03d3af6541da03
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-03-23 19:47:19 +0530

    Add files via upload

---
+++ March 2024/Reorder_List.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        }
+
+        // Find the middle of the list
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse the second half of the list
+        ListNode prev = null;
+        ListNode curr = slow.next;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        slow.next = null;
+
+        // Merge the first and second halves of the list
+        ListNode p1 = head;
+        ListNode p2 = prev;
+        while (p2 != null) {
+            ListNode next1 = p1.next;
+            ListNode next2 = p2.next;
+            p1.next = p2;
+            p2.next = next1;
+            p1 = next1;
+            p2 = next2;
+        }
+    }
+}
