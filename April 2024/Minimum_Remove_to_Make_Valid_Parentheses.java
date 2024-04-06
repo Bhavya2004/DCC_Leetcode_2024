c3b5ec8ca42c75f301226f85879d6826b0fb4f54
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-04-06 09:42:04 +0530

    Add files via upload

---
+++ April 2024/Minimum_Remove_to_Make_Valid_Parentheses.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '(') {
+                stack.push(i);
+            } else if (sb.charAt(i) == ')') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    sb.setCharAt(i, '*');
+                }
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            sb.setCharAt(stack.pop(), '*');
+        }
+
+        return sb.toString().replaceAll("\\*", "");
+    }
+}
