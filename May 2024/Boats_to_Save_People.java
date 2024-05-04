43f3583d90792b7d836c22fd9ec0df364a368a5a
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-05-04 20:53:07 +0530

    Add files via upload

---
+++ May 2024/Boats_to_Save_People.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+
+        return boats;
+        
+    }
+}
