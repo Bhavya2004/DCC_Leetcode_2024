1d8656dd2659cc028c56da82009662d11aa1b0b6
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-05-30 23:34:28 +0530

    Add files via upload

---
+++ May 2024/Count_Triplets_That_Can_Form_Two_Arrays_of_Equal_XOR.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countTriplets(int[] arr) {
+        int n = arr.length;
+        int count = 0;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int xor = arr[i];
+            
+            for (int j = i + 1; j < n; j++) {
+                xor ^= arr[j];
+                
+                if (xor == 0) {
+                    count += j - i;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
