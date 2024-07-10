1ca98b444332acaaa964f776d87f874ebe16dbad
Bhavya Barai <94623439+Bhavya2004@users.noreply.github.com>
2024-07-10 18:13:18 +0530

    Add files via upload

---
+++ July 2024/Crawler_Log_Folder.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int count = 0;
+        for (String log : logs) {
+            if (log.equals("../")) {
+                if (count > 0) {
+                    count--;
+                }
+            } else if (!log.equals("./")) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
